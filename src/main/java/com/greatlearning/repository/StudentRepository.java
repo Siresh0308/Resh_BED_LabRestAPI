f9d6cf8a36260c7684dd39513fec385878b889d0
Resh@308 <reshsingh1995@gmail.com>
2023-02-17 12:35:38 +0530

    Lab RestAPI assessment

---
+++ src/main/java/com/greatlearning/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.greatlearning.repository;
+
+import com.greatlearning.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
